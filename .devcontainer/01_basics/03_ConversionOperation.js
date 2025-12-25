50854054f8a47c95ace3e209ec8c5bf7fdd23479
Akshay Srivastava <akshaysri6981@gmail.com>
2025-12-25 13:14:27 +0000

    Conversion of Datatypes

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Akshay"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInnumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "Akshay"
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akshay" => true
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
