fdcaa518ac977f3eea64cab62a0bb68107983157
Akshay Srivastava <akshaysri6981@gmail.com>
2025-12-27 07:06:19 +0000

    05_string.js

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name = "Akshay"
+const repoCount = "10"
+
+//console.log(name + repoCount + "Value");
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String("Cricket");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+conpsole.log(gameName. toUpperCase())
+console.log(gameName.charAt(4));
+console.log(gameName.indexof('k'));
